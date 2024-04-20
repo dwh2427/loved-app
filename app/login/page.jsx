a9d83015e7c26333b67626d76f3a16b1a1d027a8
Paolo Lalap <paolomorales1502@gmail.com>
2024-04-20 13:39:32 +0800

    polished mobile

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -6,10 +6,10 @@ import { Button } from "@/components/ui/button";
 
 export default function LoginPage() {
   return (
-    <main className="mx-auto flex max-w-[766.82px] flex-col items-center justify-between px-5 xl:max-w-[1495px]">
+    <>
       <Link
         href="/"
-        className="mx-auto flex h-[183.13px] w-full flex-col items-center justify-center"
+        className="mx-auto flex h-[183.13px] w-screen max-w-[766.82px] flex-col items-center justify-center"
       >
         <div className="relative h-[118.62px] w-full max-w-[189.98px]">
           <Image
@@ -21,38 +21,37 @@ export default function LoginPage() {
           />
         </div>
       </Link>
-      <div className="flex flex-col gap-y-[41.41px]">
-        <h2 className="mx-auto max-h-[65px] whitespace-nowrap text-center text-[48.53px] font-black leading-[64.71px] tracking-[0.01em] text-black">
-          Welcome back
-        </h2>
-        <p className="mx-auto h-[38px] w-40 max-w-[582.39px] text-center text-[32.36px] font-medium leading-[37.53px] text-black">
-          Sign in
-        </p>
-        <LoginForm />
-        <p className="h-[30px] self-start text-[25.88px] font-bold leading-[29.12px]">
-          Forgot Password?
-        </p>
-        <p className="max-w-[689.17px] text-[25.88px] leading-[29.12px]">
-          By clicking the Sign In button below, you agree to the Loved{" "}
-          <span className="border-b-[0.5px] border-black">
-            Terms of Service
-          </span>
-          and acknowledge the{" "}
-          <span className="border-b-[0.5px] border-black">Privacy Notice</span>.
-        </p>
-        <p className="h-[30px] self-start text-[25.88px] leading-[29.12px]">
-          Don&apos;t have an account?{" "}
-          <Link href={""} className="font-bold">
-            Sign up
-          </Link>
-        </p>
-      </div>
-      <Button
-        variant={"default"}
-        className="mb-[42.24px] mt-[41.41px] h-[102.71px] w-full max-w-[625.75px] rounded-[64.71px] bg-[#FF007A] px-[51.77px] py-[32.36px] text-center text-[32.36px] font-black leading-[37.53px] text-[#FEFFF8] hover:bg-[#FF007A] focus:bg-[#FF007A] focus-visible:ring-0 focus-visible:ring-[#FF007A] focus-visible:ring-offset-0 dark:bg-violet-600 dark:text-gray-50"
-      >
+      <h2 className="mx-auto max-h-[65px] max-w-[325px] whitespace-nowrap text-center text-[48.53px] font-black leading-[64.71px] tracking-[0.01em] text-black">
+        Welcome back
+      </h2>
+
+      <p className="mx-auto mt-[41.41px] max-h-[38px] w-40 max-w-[582.39px] text-center text-[32.36px] font-medium leading-[37.53px] text-black">
         Sign in
-      </Button>
-    </main>
+      </p>
+      <LoginForm />
+      <p className="mt-[41.41px] h-[30px] w-full max-w-[689.17px] self-start px-5 text-[25.88px] font-bold leading-[29.12px]">
+        Forgot Password?
+      </p>
+      <p className="mt-[41.41px] w-full max-w-[689.17px] px-5 text-[25.88px] leading-[29.12px]">
+        By clicking the Sign In button below, you agree to the Loved{" "}
+        <span className="border-b-[0.5px] border-black">Terms of Service</span>
+        and acknowledge the{" "}
+        <span className="border-b-[0.5px] border-black">Privacy Notice</span>.
+      </p>
+      <p className="mt-[41.41px] h-[30px] w-full max-w-[689.17px] self-start px-5 text-[25.88px] leading-[29.12px]">
+        Don&apos;t have an account?{" "}
+        <Link href={""} className="font-bold">
+          Sign up
+        </Link>
+      </p>
+      <div className="px-5">
+        <Button
+          variant={"default"}
+          className="mb-[42.24px] mt-[41.41px] h-[102.71px] w-full max-w-[625.75px] rounded-[64.71px] bg-[#FF007A] px-[51.77px] py-[32.36px] text-center text-[32.36px] font-black leading-[37.53px] text-[#FEFFF8] hover:bg-[#FF007A] focus:bg-[#FF007A] focus-visible:ring-0 focus-visible:ring-[#FF007A] focus-visible:ring-offset-0 dark:bg-violet-600 dark:text-gray-50"
+        >
+          Sign in
+        </Button>
+      </div>
+    </>
   );
 }
--- components/LoginForm.jsx
+++ components/LoginForm.jsx
@@ -40,22 +40,23 @@ export default function LoginForm() {
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(handleSubmit)} className="flex flex-col items-center gap-y-[41.41px]">
+      <form
+        onSubmit={form.handleSubmit(handleSubmit)}
+        className="flex flex-col items-center gap-y-[41.41px] mt-[41.41px]"
+      >
         <FormField
           control={form.control}
           name="emailAddress"
           render={({ field }) => (
-            <FormItem className='w-full'>
+            <FormItem className='w-full px-5'>
               <FormLabel className="h-[30px] max-w-[160px] text-[25.88px] font-semibold leading-[29.12px] text-black">
                 Email Address
               </FormLabel>
-              <FormControl>
-                <Input
-                  placeholder="@.com"
-                  className="h-[75%] max-h-[102.71px] w-[90%] w-[689.17px] rounded-[16.18px] border-[1.94px] px-[23.3px] py-[32.36px]"
-                  {...field}
-                />
-              </FormControl>
+              <Input
+                placeholder="@.com"
+                className="h-[75%] max-h-[102.71px] w-full max-w-[689.17px] rounded-[16.18px] border-[1.94px] px-[23.3px] py-[32.36px]"
+                {...field}
+              />
               <FormMessage />
             </FormItem>
           )}
@@ -64,17 +65,15 @@ export default function LoginForm() {
           control={form.control}
           name="password"
           render={({ field }) => (
-            <FormItem className='w-full'>
+            <FormItem className='w-full px-5'>
               <FormLabel className="h-[30px] w-[160px] text-[25.88px] font-semibold leading-[29.12px] text-black">
                 Password
               </FormLabel>
-              <FormControl>
-                <Input
-                  placeholder="@.com"
-                  className="h-[75%] max-h-[102.71px] w-[90%] w-[689.17px] rounded-[16.18px] border-[1.94px] px-[23.3px] py-[32.36px]"
-                  {...field}
-                />
-              </FormControl>
+              <Input
+                placeholder="@.com"
+                className="h-[75%] max-h-[102.71px] w-full max-w-[689.17px] rounded-[16.18px] border-[1.94px] px-[23.3px] py-[32.36px]"
+                {...field}
+              />
               <FormMessage />
             </FormItem>
           )}
