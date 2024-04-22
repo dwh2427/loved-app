01a5b4d4a3f7ebb7d91d133b7ecc5764309f17fc
Paolo Lalap <paolomorales1502@gmail.com>
2024-04-22 14:18:39 +0800

    add firebase

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,35 +1,10 @@
-// DashboardPage.js
-"use client";
-import { useEffect } from "react";
-import { useAuthState } from "react-firebase-hooks/auth";
-import { auth } from "@/firebase/config";
-import { useRouter } from "next/navigation";
-import { signOut } from "firebase/auth";
+import LogoutButton from "./logout-button.jsx";
 
 export default function DashboardPage() {
-  const [user] = useAuthState(auth);
-  const router = useRouter();
-
-  useEffect(() => {
-    if (!user) {
-      const userSession = sessionStorage.getItem("user");
-      if (!userSession) {
-        router.push("/login");
-      }
-    }
-  }, [user, router]);
-
   return (
     <>
+      <LogoutButton />
       <div>dashboard</div>
-      <button
-        onClick={() => {
-          signOut(auth);
-          sessionStorage.removeItem("user");
-        }}
-      >
-        Log out
-      </button>
     </>
   );
 }
