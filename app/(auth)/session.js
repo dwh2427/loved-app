4b862997cecc3df3e46ea9335816d781beb3c4c9
Paolo Lalap <paolomorales1502@gmail.com>
2024-04-24 23:02:37 +0800

    revert back

--- app/(auth)/session.js
+++ app/(auth)/session.js
@@ -1,31 +1,20 @@
-'use client';
-import { useEffect, useState } from "react";
+"use client";
+import { useEffect } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "@/firebase/config";
 import { useRouter } from "next/navigation";
-import Loading from "@/components/loading/loading";
 
 export default function Session({ children }) {
   const [user] = useAuthState(auth);
   const router = useRouter();
-  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    if (!user && typeof window !== "undefined") {
+    if (!user) {
       const userSession = sessionStorage.getItem("user");
       if (!userSession) {
         router.push("/login");
-      } else {
-        setIsLoading(false);
       }
-    } else {
-      setIsLoading(false);
     }
   }, [user, router]);
-
-  if (isLoading) {
-    return <Loading />;
-  }
-
   return children;
 }
--- app/login/session.js
+++ app/login/session.js
@@ -1,26 +1,20 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "@/firebase/config";
 import { useRouter } from "next/navigation";
-import Loading from "@/components/loading/loading";
 
 export default function Session({ children }) {
   const [user] = useAuthState(auth);
   const router = useRouter();
-  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    if (
-      user &&
-      typeof window !== "undefined" &&
-      sessionStorage.getItem("user")
-    ) {
-      router.push("/dashboard");
-    } else {
-      setIsLoading(false);
+    if (user) {
+      const userSession = sessionStorage.getItem("user");
+      if (userSession) {
+        router.push("/dashboard");
+      }
     }
   }, [user, router]);
-
-  return isLoading ? <Loading /> : <>{children}</>;
+  return children;
 }
