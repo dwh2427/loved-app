0c2876018cf9d4f664c5b83c5fcc81bc98cd0316
Paolo Lalap <paolomorales1502@gmail.com>
2024-04-28 21:30:36 +0800

    update button

--- components/button/logout.jsx
+++ components/button/logout.jsx
@@ -1,15 +1,20 @@
 "use client";
 import { signOut } from "firebase/auth";
 import { auth } from "@/firebase/config";
+import { cn } from "@/lib/utils";
 
-export default function LogoutButton() {
+export default function LogoutButton({ className }) {
   return (
     <button
       onClick={() => {
         signOut(auth);
         sessionStorage.removeItem("user");
       }}
-      className="mx-auto h-[20px] w-max text-center text-[18px] font-normal leading-[20px] text-[#004318]"
+      tabIndex={"-1"}
+      className={cn(
+        `mx-auto h-[20px] w-max text-center text-[18px] font-normal leading-[20px] text-[#004318]`,
+        className,
+      )}
     >
       Log out
     </button>
