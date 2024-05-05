f7b71feefa3a66fa9f965f5a2c96d5d7c168de7f
Paolo Lalap <paolomorales1502@gmail.com>
2024-05-05 13:15:10 +0800

    add loading ui

--- components/form/sign-up.jsx
+++ components/form/sign-up.jsx
@@ -19,6 +19,7 @@ import { z } from "zod";
 import { Button } from "../ui/button";
 import { useRouter } from "next/navigation";
 import { createUserWithEmailAndPassword } from "firebase/auth";
+import { Loader2 } from "lucide-react";
 
 const formSchema = z.object({
   firstName: z.string().min(1, {
@@ -119,7 +120,6 @@ export default function SignUpForm() {
           localStorage.removeItem("familyMemberType");
         }
       }
-      setLoading(false);
     } catch (e) {
       console.error(e);
       setLoading(false);
@@ -247,6 +247,7 @@ export default function SignUpForm() {
           disabled={loading}
           className="mx-auto h-[102.71px] w-full max-w-[625.75px] rounded-[64.71px] bg-[#FF007A] px-[51.77px] py-[32.36px] text-center text-[32.36px] font-black leading-[37.53px] text-[#FEFFF8] hover:bg-[#FF007A] focus:bg-[#FF007A] focus-visible:ring-0 focus-visible:ring-[#FF007A] focus-visible:ring-offset-0 dark:bg-violet-600 dark:text-gray-50 md:mt-[16px] md:h-[62px] md:w-[384px] md:rounded-[100px] md:px-[25px] md:py-[20px] md:text-center md:text-[18px] md:font-black md:leading-[22px]"
         >
+          {loading && <Loader2 className="mr-2 size-6 animate-spin" />}
           Sign up
         </Button>
       </form>
