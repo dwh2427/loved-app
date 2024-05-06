31ff7aab93dad5ff6bbfbad761d2678a04ef40f3
Paolo Lalap <paolomorales1502@gmail.com>
2024-05-06 20:19:20 +0800

    polished dash

--- components/header/landing.jsx
+++ components/header/landing.jsx
@@ -32,7 +32,7 @@ export default function Header({ logo = "" }) {
             className="xl:h-[74.01px] xl:w-[118.48px]"
           />
         </Link>
-        <div className="hidden h-[62px] w-[301px] flex-shrink-0 items-center gap-x-[32px] md:flex">
+        <div className="hidden h-[62px] flex-shrink-0 items-center gap-x-[32px] md:flex">
           {user ? (
             <Link
               href={"/dashboard"}
