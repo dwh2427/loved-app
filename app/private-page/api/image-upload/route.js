c92914adeb02008f2cf1febc681d5ee6a95bee09
Saidur Rohman <saidhstu@gmail.com>
2024-05-08 13:21:00 +0600

    file upload issue fixed

--- app/private-page/api/image-upload/route.js
+++ app/private-page/api/image-upload/route.js
@@ -3,22 +3,56 @@ import { uploadFileToGCS } from "@/lib/uploadFIleToGCS";
 import Loved from "@/models/loved";
 export const POST = async (request) => {
   try {
-    // Parse incoming form data to get the uploaded file
-    // const body = await request.json();
     const form = await request.formData();
     const file = form.get("file");
     const username = form.get("username");
     const uid = form.get("uid");
-    if (!file && !username && !uid)
-      return createError("required params is missing", 400);
 
-    const imageLink = await uploadFileToGCS(file);
-    const update = await Loved.findOneAndUpdate(
-      { username, uid },
-      { $push: { images: imageLink } },
-      { new: true }, // To return the updated document
-    );
-    return Response.json({ data: update, message: "Uploaded successfully" });
+    if (!file || !username || !uid)
+      return createError("Required parameters are missing", 400);
+    // console.log(file.type);
+    if (file.type === "application/octet-stream") {
+      // Convert HEIC to JPEG
+      // Dynamically import the module
+      // const heicConvert = await import("heic-convert");
+      // const fs = await import("fs");
+      // const arrayBuffer = await file.arrayBuffer();
+      // const jpegBuffer = await heicConvert({
+      //   buffer: arrayBuffer,
+      //   format: "JPEG",
+      //   quality: 1, // Quality 1 (highest) will maintain original quality
+      // });
+
+      // // Save the JPEG file locally
+      // const localFilePath = path.join("uploads", `${file.name}.jpeg`);
+      // await writeFile(localFilePath, jpegBuffer);
+
+      // // Proceed with cloud upload and database update
+      // // const imageLink = await uploadFileToGCS(file); // Uploading HEIC file to GCS
+      // const jpegFile = new File([jpegBuffer], `${file.name}.jpeg`, {
+      //   type: "image/jpeg",
+      // });
+      // console.log(jpegFile);
+
+      const jpegImageLink = await uploadFileToGCS(file); // Uploading JPEG file to GCS
+      const update = await Loved.findOneAndUpdate(
+        { username, uid },
+        { $push: { images: jpegImageLink } },
+        { new: true }, // To return the updated document
+      );
+
+      return Response.json({ data: update, message: "Uploaded successfully" });
+    } else {
+      // If file type is not HEIC, directly upload to cloud and update database
+      const imageLink = await uploadFileToGCS(file);
+      const update = await Loved.findOneAndUpdate(
+        { username, uid },
+        { $push: { images: imageLink } },
+        { new: true }, // To return the updated document
+      );
+
+      return Response.json({ data: update, message: "Uploaded successfully" });
+    }
   } catch (error) {
     return errorResponse(error);
   }
--- app/private-page/page.jsx
+++ app/private-page/page.jsx
@@ -119,7 +119,6 @@ export default function PrivatePage() {
       } catch (error) {
         handleClientError(error)
       } finally { setIsUploading(false) }
-
     }
   };
 
