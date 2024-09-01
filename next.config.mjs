b53931124686aa3c904da68fc6f38eb9f5371e96
Saidur Rohman <saidhstu@gmail.com>
2024-09-01 14:29:32 +0600

    all issue fixed

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ["storage.googleapis.com", "upload.wikimedia.org", "i.ibb.co"],
-  },
-  reactStrictMode: true,
-};
-
-export default nextConfig;
