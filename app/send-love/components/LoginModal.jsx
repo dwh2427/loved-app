85cdbde541600a3fc256ec612a2f34598d790668
Saidur Rohman <saidhstu@gmail.com>
2024-10-13 15:59:00 +0600

    login modal udpated

--- app/send-love/components/LoginModal.jsx
+++ app/send-love/components/LoginModal.jsx
@@ -90,7 +90,7 @@ export default function LoginModal({ isOpen, setOnCloseLogin }) {
   return (
 
 
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 custom-popup">
+    <div className="fixed inset-0 bg-dark bg-opacity-40 flex items-center justify-center z-50 custom-popup custom-overlay">
       <div className="flex lg:w-[64rem] lg:h-[39rem] mx-auto rounded-lg shadow-lg overflow-hidden bg-white">
         {/* Left Side - Sign In Form*/}
         <div className="w-full lg:w-1/2 flex flex-col items-center justify-center px-8">
@@ -120,7 +120,8 @@ export default function LoginModal({ isOpen, setOnCloseLogin }) {
                     onSubmit={form.handleSubmit(handleSubmit)}
                     className="mt-[41.41px] flex flex-col items-center gap-y-[17.41px] md:gap-y-[41px]"
                 >
-                    <div className="space-y-41.41px md:mt-16px md:max-w-385px mx-auto w-full md:flex md:space-y-0">
+                  <div className="space-y-41.41px md:mt-16px md:max-w-385px mx-auto w-full md:space-y-0">
+                    <label htmlFor="phone" className="phone-input-label font-plus-jakarta-sans">Enter your phone number</label>
                     <FormField
                         control={form.control}
                         name={"phone"}
@@ -130,29 +131,35 @@ export default function LoginModal({ isOpen, setOnCloseLogin }) {
                             <PhoneInput
                                 country={'au'}
                                 inputStyle={{
-                                // width: "calc(100% - 20px)",
-                                width:"100%",
-                                height: "50px",
-                                borderRadius: "32px",
-                                paddingLeft: "70px", // Ensure text does not overlap with button
+                                  width:"100%",
+                                  height: "48px",
+                                  paddingLeft: "80px", // Ensure text does not overlap with button
+                                  borderRadius: "var(--Spacing-3, 12px)",
+                                  border: "1px solid var(--Light-Mode-Border-Secondary, #D8D8DA)",
+                                  background: "var(--Light-Mode-Background-Default, #FFF)"
                                 }}
                                 buttonStyle={{
-                                width: "70px", // Set the width for the code holder
-                                borderTopLeftRadius: "32px",
-                                borderBottomLeftRadius: "32px",
-                                position: "absolute", // Position button absolutely to prevent overlap
-                                zIndex: 1, // Ensure button appears above the input
+                                  position: "absolute", // Position button absolutely to prevent overlap
+                                  zIndex: 1, // Ensure button appears above the input
+                                  display: "flex",
+                                  padding: "0px 10px 0px 8px",
+                                  alignItems: "center",
+                                  gap: "var(--Spacing-1, 4px)",
+                                  alignSelf: "stretch",
+                                  border: "1px solid var(--Light-Mode-Border-Secondary, #D8D8DA)",
+                                  background: "var(--Light-Mode-Background-Secondary, #F8F9FB)",
+                                  borderRadius: "var(--Spacing-3, 12px) 0px 0px var(--Spacing-3, 12px)",
                                 }}
                                 containerStyle={{
-                                position: "relative", // Set container to relative to position button properly
+                                  position: "relative", // Set container to relative to position button properly
                                 }}
-                                className="phone-input-custom mt-[8px] w-full"
+                                className="phone-input-custom mt-[8px] w-full phone-input-text"
                                 placeholder="Phone Number"
                                 {...field}
                                 inputExtraProps={{
-                                ref,
-                                required: true,
-                                autoFocus: true,
+                                  ref,
+                                  required: true,
+                                  autoFocus: true,
                                 }}
                             />
 
@@ -167,7 +174,7 @@ export default function LoginModal({ isOpen, setOnCloseLogin }) {
                     type="submit"
                     disabled={loading}
                     variant={"default"}
-                    className="signBtn mx-auto h-[52px] w-full max-w-[625.75px] text-base font-semibold rounded-[64.71px] bg-[#FF007A] px-[51.77px] py-[32.36px] text-center text-[32.36px] leading-[37.53px] text-[#FEFFF8] hover:bg-[#FF007A] focus:bg-[#FF007A] focus-visible:ring-0 focus-visible:ring-[#FF007A] focus-visible:ring-offset-0 dark:bg-violet-600 dark:text-gray-50 md:h-[52px] md:w-[450px] md:rounded-[100px] md:px-[25px] md:py-[20px] md:text-center md:text-[18px] md:leading-[22px]"
+                    className="font-plus-jakarta-sans signBtn mx-auto h-[52px] w-full max-w-[625.75px] text-base font-semibold rounded-[64.71px] bg-[#FF007A] px-[51.77px] py-[32.36px] text-center text-[32.36px] leading-[37.53px] text-[#FEFFF8] hover:bg-[#FF007A] focus:bg-[#FF007A] focus-visible:ring-0 focus-visible:ring-[#FF007A] focus-visible:ring-offset-0 dark:bg-violet-600 dark:text-gray-50 md:h-[52px] md:w-[450px] md:rounded-[100px] md:px-[25px] md:py-[20px] md:text-center md:text-[18px] md:leading-[22px]"
                     >
                     {loading && <Loader2 className="mr-2 size-6 animate-spin" />}
                     Sign In
