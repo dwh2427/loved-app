5b32cce668f365f11fbf60a9effba2cc35f54c85
Saidur Rohman <saidhstu@gmail.com>
2024-10-16 17:15:19 +0600

    sending message pop up updated

--- app/send-love/components/ConfirmPopup.jsx
+++ app/send-love/components/ConfirmPopup.jsx
@@ -1,5 +1,6 @@
-import { FaHeart } from "react-icons/fa";
+import Image from "next/image";
 import { useState } from "react";
+import ComplexHeart from "@/public/complex-heart.svg";
 
 export default function PopupMessage() {
   const [isVisible, setIsVisible] = useState(true);
@@ -8,14 +9,15 @@ export default function PopupMessage() {
    
         <div className={`fixed inset-0 schedule-popup-overlay flex items-center justify-center ${!isVisible && 'hidden'}`}>
         <div className="bg-gray-50 shadow-lg rounded-lg p-8 w-96 text-center">
-            <div className="text-xl font-semibold mb-4">Sending your message...</div>
+            <div className="sending-message-text mb-4 sending-message-1024">Sending your message...</div>
+            <div className="sending-message-text mb-4 sending-message-1023">Sending your <br/> message...</div>
             <div className="flex justify-center my-4">
-            <FaHeart className="text-pink-500" size={50} />
+            <Image src={ComplexHeart} className="text-pink-500" size={50} />
             </div>
-            <div className="text-gray-500 italic">
+            <div className="speech-text">
             “A grateful perspective brings happiness and abundance into a person life.”
             </div>
-            <div className="text-gray-500 mt-2">— Unknown</div>
+            <div className="author-name mt-2">— Unknown</div>
         </div>
         </div>
 
